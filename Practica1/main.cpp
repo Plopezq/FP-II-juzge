295ff81268ed855f18fa7aa59ebb48b5ded97852
Plopezq <lopezmartinpablo@gmail.com>
2020-03-03 17:54:53 +0100

    Commit hoja 9

--- Practica1/main.cpp
+++ Practica1/main.cpp
@@ -1,5 +1,6 @@
 #include "juego.cpp" //Que este incluye juego.h y ese incluye mina.h
-
+#include<conio.h>
+#include<stdio.h>
 using namespace std;
 
 int main() {
@@ -13,3 +14,33 @@ void colorFondo(int color) {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(handle, 15 | (color << 4));
 }
+
+
+tTecla leerTecla{
+	int dir;
+	cin.sync();
+	dir = _getch(); // dir: tipo int
+	if (dir == 0xe0) {
+ dir = _getch();
+ // ...
+
+// Si aquí dir es 27, es la tecla Esc
+
+	/*
+		Dirección de las flechas
+		arriba 72
+		abajo 80
+		derecha	77
+		izquierda 75
+	*/
+
+}
+
+bool hacerMovimiento(tJuego& juego, tTecla tecla) {
+
+
+}
+
+
+
+
--- Practica1/mina.h
+++ Practica1/mina.h
@@ -8,8 +8,27 @@ typedef struct {
 	int posFila, posColumna; //Posicion donde se encuentra el minero.
 }tMina;
 
+void cargar_Mina(ifstream& fichero, tMina& mina) {
+	//lee	los	datos	de	un	fichero y	guarda	la	posición	del	minero.
+
+}
+
+
+void dibujar1_1(const tMina& mina) { //: dibuja	la	mina	a	escala	1 : 1
+
+
+}
+
+void dibujar1_3(const tMina& mina) { 
+//: dibuja la mina a escala 1:3	y usa la siguientefunción dibuja3x3.
 
 
 
+}
+
+void dibuja3x3(tCasilla casilla, tPlanoCaracteres caracteres, tPlanoColores colores, int i, int j){
+	// : dibuja	las	casillas aumentadas	tres veces. 
+	// En concreto, la	casilla	sirve para actualizar el plano de caracteres y colores en las
+	// coordenadas	i, j.
 
 
