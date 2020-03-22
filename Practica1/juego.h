8324a19cf3bdc50c4b87f6b7abac44f08f618006
Pablo Lopez Martin <pablop14@ucm.es>
2020-03-22 19:17:17 +0100

    Commit

--- Practica1/juego.h
+++ Practica1/juego.h
@@ -1,13 +1,21 @@
 #include "mina.h"
 
 typedef enum {ARRIBA, ABAJO, DCHA, IZDA, SALIR, NADA, TNT} tTecla;
-typedef struct {
+typedef enum {
+	EXPLORANDO, //El minero continua buscando diamantes por la mina
+	EXITO, //El minero consigue llegar a la salida
+	ABANDONO, //Si el minero decide sentarse a esperar a que le vengan a rescatar 
+	FRACASO //Si el minero muero durante la exploración de la mina
+}tEstado;
 
+typedef struct {
 	tMina mina;
 	int contGemas;
 	int contMov;
-	int contTNT;
-
+	tEstado estadoMinero;
+	int escalaJuego; //La opcion seleccionada para dibujar el juego 1: escala 1:1 y 2 escala 1:3
+	int opcionMov; //Opcion elegida para introducir movimientos: por teclado (1) o por fichero (2)
+	//int contTNT; 
 }tJuego;
 
 //PROTOTIPOS
